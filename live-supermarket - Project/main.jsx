2606339bb32ff1f7080bb4f35b8e5171344a57e2
muhsinmanuppa <muhsinmanuppa@gmail.com>
2024-12-29 23:22:36 +0530

    Fix import of app.css and update main.jsx

--- live-supermarket - Project/main.jsx
+++ live-supermarket - Project/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app.jsx';
-import './src/styles/app.css';
+import './app.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
